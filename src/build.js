e789a10dead484d9f3dfbd13d9b570f1c9d891f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-06-23 10:35:16 -0400

    finding spec files

--- src/build.js
+++ src/build.js
@@ -4,47 +4,52 @@ const debug = require('debug')('multi')
 // const path = require('path')
 // const relative = path.join.bind(null, __dirname)
 // const inCurrent = path.join.bind(null, process.cwd())
-const configs = require('./find-specs')
-const rollem = require('rollem')
-const R = require('ramda')
+// const configs = require('./find-specs')
+// const rollem = require('rollem')
+// const R = require('ramda')
 
 const config = require('./get-my-config')('multi-cypress')
 if (!config) {
   console.error('Cannot find package.json > config > multi-cypress object')
-  process.exti(1)
+  process.exit(1)
 }
 debug('multi-cypress config', config)
 
-function isWatchArgument (arg) {
-  return arg === '-w' || arg === '--watch'
-}
-const options = {
-  watch: process.argv.some(isWatchArgument)
-}
+const findSpecs = require('./find-specs')
+const inputFiles = findSpecs(config.specs)
+debug('input spec files')
+debug(inputFiles)
 
-const generateGitLabCiFile = require('./generate-gitlab-file')
+// function isWatchArgument (arg) {
+//   return arg === '-w' || arg === '--watch'
+// }
+// const options = {
+//   watch: process.argv.some(isWatchArgument)
+// }
 
-function buildError (err) {
-  console.error('Could not build everything')
-  if (err.message) {
-    console.error(err.message)
-  }
-  if (err.stack) {
-    console.error(err.stack)
-  }
-}
+// const generateGitLabCiFile = require('./generate-gitlab-file')
 
-function exit () {
-  process.exit(1)
-}
+// function buildError (err) {
+//   console.error('Could not build everything')
+//   if (err.message) {
+//     console.error(err.message)
+//   }
+//   if (err.stack) {
+//     console.error(err.stack)
+//   }
+// }
 
-if (options.watch) {
-  rollem(configs, options)
-    .then((roller) => {
-      roller.on('rolled', generateGitLabCiFile)
-    })
-} else {
-  rollem(configs, options)
-    .then(generateGitLabCiFile)
-    .catch(R.pipe(buildError, exit))
-}
+// function exit () {
+//   process.exit(1)
+// }
+
+// if (options.watch) {
+//   rollem(configs, options)
+//     .then((roller) => {
+//       roller.on('rolled', generateGitLabCiFile)
+//     })
+// } else {
+//   rollem(configs, options)
+//     .then(generateGitLabCiFile)
+//     .catch(R.pipe(buildError, exit))
+// }
--- src/get-my-config.js
+++ src/get-my-config.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('multi')
 const la = require('lazy-ass')
 const is = require('check-more-types')
 const inCurrent = require('path').join.bind(null, process.cwd())
@@ -7,11 +8,14 @@ const inCurrent = require('path').join.bind(null, process.cwd())
 function getMyConfig (name) {
   la(is.unemptyString(name), 'missing name to look for')
   const packagePath = inCurrent('package.json')
+  debug('reading', packagePath)
   const pkg = require(packagePath)
   if (!is.object(pkg.config)) {
+    debug(`cannot find config object in ${packagePath}`)
     return
   }
   if (!is.object(pkg.config[name])) {
+    debug(`cannot find config ${name} object in ${packagePath}`)
     return
   }
   return pkg.config[name]
