06f00fc231f1c60ce8c69f9ba70e716bbba9c727
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-06-23 13:55:36 -0400

    chore(example): test has example with ES6 import

---
+++ test/src/add.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export function add (a, b) {
+  return a + b
+}
---
+++ test/src/one-spec.js
@@ -0,0 +1,5 @@
+'use strict'
+
+// this is an example spec that includes another file
+import {add} from './add'
+console.log('2 + 3 =', add(2, 3))
